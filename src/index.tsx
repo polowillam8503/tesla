85cc1fa0b964cc415fd7836eb4b991867b9c90ae
Zhuanz密码0000 <Zhuanz@MacBook-Pro.local>
2025-12-03 10:19:33 +0800

    Fix white screen: clean html and import css

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element");
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
